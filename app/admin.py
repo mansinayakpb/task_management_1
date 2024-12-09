88dd39edb76a39e3c5bf1124c86f7c487fa86ff5
mansi <mnayak@bestpeers.com>
2024-12-09 16:33:23 +0530

    register the models in admin file

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Task, Comment
 
 # Register your models here.
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = (
+        "id",
+        "email",
+        "first_name",
+        "last_name",
+    )
+    search_fields = ("email", "first_name", "last_name")
+
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "text", "user")
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "status",
+        "title",
+        "detail",
+        "priority",
+        "assigned_by",
+        "assigned_to",
+        "priority",
+    )
